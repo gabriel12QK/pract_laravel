957ee57a0c728e97ec4be3219f7c5fb3037328ac
gabriel12QK <jose.rivas@espam.edu.ec>
2022-07-04 22:17:55 -0500

    tipo_completo

--- app/Http/Livewire/Usuario.php
+++ app/Http/Livewire/Usuario.php
@@ -6,10 +6,11 @@ use Livewire\Component;
 use App\Models\tipo;
 class Usuario extends Component
 {
-    public $tipos;
+    public $tipos,$_id;
     public function render()
     {
-        return view('livewire.usuario');
+        $t=tipo::where('estado',1)->get();
+        return view('livewire.usuario', compact('t'));
     }
      
     public function guardar(){
@@ -19,4 +20,26 @@ class Usuario extends Component
         $tipos->save();
     }
 
+    public function edit($id){
+        $tipos= tipo::find($id);
+        $this->_id = $id;
+        $this->tipos=$tipos->tipo;//mostrar el dato al realizar la busqueda primero ubicamos el nombre del cuadro de texto luego la variable y por ultimo el nombre de la columna
+    }
+
+    public function update(){
+        $tipos =tipo::find( $this->_id);
+        $tipos->update([
+            'tipo' => $this->tipos
+        ]);
+        $this->reset();
+    }
+
+    public function destroyL($id){
+        
+        $tipos = tipo::find($id);
+        $tipos->update([
+            'estado' => 0
+        ]);
+        $this->reset();
+    }
 }
--- resources/views/livewire/usuario.blade.php
+++ resources/views/livewire/usuario.blade.php
@@ -10,8 +10,48 @@
                     <label class="form-label">Tipo</label>
                     <input type="text" class="form-control" placeholder="" wire:model="tipos">
                </div>
-                <button type="button" class="btn btn-primary" wire:click="guardar">guardar</button>
+           
+               <button type="button" class="btn btn-primary" wire:click="guardar">guardar</button>
+              
+               <button type="button" class="btn btn-primary" wire:click="update">actualizar</button>
+              
+                
     </div>
 </div>
 </div>
+<div class="row justify-content-center">
+    <div class="col-10" >
+    <div class="card">
+        <div class="card-body">
+ 
+            <table id="datatables-reponsive" class="table table-striped" style="width:75%" >
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Tipos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($t as $item)
+                    <tr>
+                        <td>
+                           {{$item->id}}
+                        </td>
+                        <td>
+                            {{$item->tipo}}
+                        </td>
+                        <td class="table-action">
+                            
+                            <a ><i class="align-middle fas fa-fw fa-pen" wire:click="edit({{ $item->id }})" style="cursor: pointer "></i></i></a>
+                            <a ><i class="align-middle fas fa-fw fa-trash " wire:click="destroyL({{$item->id}})" style="cursor: pointer "></i></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+    </div>
+    </div>
 </div>
